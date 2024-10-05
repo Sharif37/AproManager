fd13a23060c41381c68c80c5384bb177e862b3d9
sharif37 <sharif.csecu@gmail.com>
2024-10-06 02:12:01 +0600

    added option for review data fetch

--- android-frontend/AproManager/app/src/main/java/com/example/AproManager/kotlinCode/GraphQl/RetrofitClientForGraphQl.java
+++ android-frontend/AproManager/app/src/main/java/com/example/AproManager/kotlinCode/GraphQl/RetrofitClientForGraphQl.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClientForGraphQl {
 
-    private static final String baseUrl="https://cfb9-103-134-127-177.ngrok-free.app/";
+    private static final String baseUrl="https://b542-103-134-127-176.ngrok-free.app/";
     private static final Retrofit retrofit=new Retrofit.
             Builder().
             baseUrl(baseUrl)
--- android-frontend/AproManager/app/src/main/java/com/example/AproManager/kotlinCode/activities/AboutUsActivity.kt
+++ android-frontend/AproManager/app/src/main/java/com/example/AproManager/kotlinCode/activities/AboutUsActivity.kt
@@ -3,6 +3,8 @@ package com.example.AproManager.kotlinCode.activities
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.AproManager.databinding.ActivityAboutUsBinding
@@ -12,10 +14,7 @@ import com.example.AproManager.kotlinCode.models.GraphQLRequest
 import com.example.AproManager.kotlinCode.models.Review
 import com.example.AproManager.kotlinCode.retrofit.RetrofitClient
 import com.example.AproManager.kotlinCode.retrofit.RetrofitClient2
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import com.google.gson.JsonObject
 import okhttp3.ResponseBody
 import org.json.JSONArray
@@ -24,14 +23,10 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-
 class AboutUsActivity : AppCompatActivity() {
-
 
     private lateinit var adapter: ReviewAdapter
     private var reviewList: ArrayList<Review> = ArrayList()
-    private var query: String = ""
-
     private lateinit var binding: ActivityAboutUsBinding
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -40,129 +35,81 @@ class AboutUsActivity : AppCompatActivity() {
         binding = ActivityAboutUsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         binding.webView.settings.javaScriptEnabled = true
         binding.mapWebView.settings.javaScriptEnabled = true
 
         loadVideo()
         loadMap()
 
-        val avgRatingTextView = binding.avgRating
-        val totalUserGiveRateTextView = binding.totalUserGiveRate
+        setupReviewSourceSelector()
 
-        // Bind RatingBar
-        val ratingBar = binding.ratingBar
-
-        // Bind ProgressBars
-        val progressBar5 = binding.progressBar5
-        val progressBar4 = binding.progressBar4
-        val progressBar3 = binding.progressBar3
-        val progressBar2 = binding.progressBar2
-        val progressBar1 = binding.progressBar1
-
-
-         getAllReviewsFromDatabase { reviewList ->
-
-             val averageRating = reviewList.map { it.rating }.average()
-             avgRatingTextView.text = String.format("%.1f", averageRating)
-             ratingBar.rating = averageRating.toFloat()
-
-             // Update total user rating count
-             val totalUserRatingCount = reviewList.size
-             totalUserGiveRateTextView.text = totalUserRatingCount.toString()
-
-             // Calculate rating distribution
-             val ratings = reviewList.map { it.rating.toInt() }
-             val ratingCounts =
-                 IntArray(5) // Array to store count of ratings for each star (0-4 stars)
-
-             for (rating in ratings) {
-                 ratingCounts[rating - 1]++ // Increment count for corresponding rating
-             }
-
-             // Update progress bars for each rating
-             progressBar5.progress =
-                 calculateProgressBarProgress(ratingCounts[4], totalUserRatingCount)
-             progressBar4.progress =
-                 calculateProgressBarProgress(ratingCounts[3], totalUserRatingCount)
-             progressBar3.progress =
-                 calculateProgressBarProgress(ratingCounts[2], totalUserRatingCount)
-             progressBar2.progress =
-                 calculateProgressBarProgress(ratingCounts[1], totalUserRatingCount)
-             progressBar1.progress =
-                 calculateProgressBarProgress(ratingCounts[0], totalUserRatingCount)
-
-
-             adapter = ReviewAdapter(reviewList, this@AboutUsActivity)
-             binding.reviewRecyclerView.layoutManager = LinearLayoutManager(this)
-             binding.reviewRecyclerView.adapter = adapter
-         }
-
-
-        /* getAllReviewsFromRestApi { reviews, throwable ->
-            Log.d("AboutUsActivity",reviews.toString())
-            if(reviews!=null) {
-                adapter = ReviewAdapter(reviews, this@AboutUsActivity)
-                binding.reviewRecyclerView.layoutManager = LinearLayoutManager(this)
-                binding.reviewRecyclerView.adapter = adapter
-            }else
-            {
-                Log.d("AboutUsActivity",throwable.toString())
+        binding.fetchDataButton.setOnClickListener {
+            val selectedSource = binding.dataSourceSpinner.selectedItem.toString()
+            when (selectedSource) {
+                "Firebase" -> getAllReviewsFromDatabase { reviews ->
+                    displayReviews(reviews)
+                }
+                "REST API" -> getAllReviewsFromRestApi { reviews, throwable ->
+                    if (reviews != null) {
+                        displayReviews(reviews)
+                    } else {
+                        showError(throwable)
+                    }
+                }
+                "GraphQL" -> getAllReviewsFromGraphQl { reviews, throwable ->
+                    if (reviews != null) {
+                        displayReviews(reviews)
+                    } else {
+                        showError(throwable)
+                    }
+                }
+                else -> Toast.makeText(this, "Please select a valid source", Toast.LENGTH_SHORT).show()
             }
-
-        }*/
-
-
-        /* getAllReviewsFromGraphQl { reviews, throwable ->
-            if (reviews != null) {
-                adapter = ReviewAdapter(reviews, this@AboutUsActivity)
-                binding.reviewRecyclerView.layoutManager = LinearLayoutManager(this)
-                binding.reviewRecyclerView.adapter = adapter
-            } else {
-                Log.d("AboutUsActivity", throwable.toString())
-            }
-
-
-        }*/
+        }
     }
 
+    private fun setupReviewSourceSelector() {
+        val sources = listOf("Select Source", "Firebase", "REST API", "GraphQL")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, sources)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.dataSourceSpinner.adapter = adapter
+    }
 
-    private fun calculateProgressBarProgress(ratingCount: Int, totalUserRatingCount: Int): Int {
-        return if (totalUserRatingCount > 0) {
-            ((ratingCount.toFloat() / totalUserRatingCount) * 100).toInt()
-        } else {
-            0
-        }
+    private fun displayReviews(reviews: ArrayList<Review>) {
+        adapter = ReviewAdapter(reviews, this@AboutUsActivity)
+        binding.reviewRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.reviewRecyclerView.adapter = adapter
+    }
+
+    private fun showError(throwable: Throwable?) {
+        Toast.makeText(this, "Error fetching reviews: ${throwable?.message}", Toast.LENGTH_LONG).show()
+        Log.e("AboutUsActivity", "Error fetching reviews", throwable)
     }
 
     private fun loadVideo() {
         val videoId = "Bcid33tgq8A"
         val frameVideo = """
-    <!DOCTYPE html>
-    <html>
-    <body>
-    <iframe width="400dp" height="300dp" src="https://www.youtube.com/embed/$videoId" frameborder="0" allow="autoplay; encrypted-media" splashscreen></iframe>
-    </body>
-    </html>
-""".trimIndent()
+            <!DOCTYPE html>
+            <html>
+            <body>
+            <iframe width="400dp" height="300dp" src="https://www.youtube.com/embed/$videoId" frameborder="0" allow="autoplay; encrypted-media" splashscreen></iframe>
+            </body>
+            </html>
+        """.trimIndent()
 
         binding.webView.loadData(frameVideo, "text/html", "utf-8")
     }
 
     private fun loadMap() {
-        val frameMap =
-            """<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d460.8617979104278!2d91.7856623!3d22.4706029!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30acd6fcf93fffff%3A0x12b289338778d80f!2sIT%20Building!5e0!3m2!1sen!2sbd!4v1709773506963!5m2!1sen!2sbd" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>"""
+        val frameMap = """
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d460.8617979104278!2d91.7856623!3d22.4706029!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30acd6fcf93fffff%3A0x12b289338778d80f!2sIT%20Building!5e0!3m2!1sen!2sbd!4v1709773506963!5m2!1sen!2sbd" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        """.trimIndent()
         binding.mapWebView.loadData(frameMap, "text/html", "utf-8")
     }
 
-
-
-    //TODO get ALl review from firebase
     private fun getAllReviewsFromDatabase(callback: (ArrayList<Review>) -> Unit) {
-        val database =
-            FirebaseDatabase.getInstance("https://apromanager-972c5-default-rtdb.asia-southeast1.firebasedatabase.app/")
+        val database = FirebaseDatabase.getInstance("https://apromanager-972c5-default-rtdb.asia-southeast1.firebasedatabase.app/")
         val ref = database.getReference("reviews")
-
         val reviewList = ArrayList<Review>()
 
         ref.addListenerForSingleValueEvent(object : ValueEventListener {
@@ -177,17 +124,13 @@ class AboutUsActivity : AppCompatActivity() {
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
-                // Handle errors here
-                callback(reviewList) // Return empty list in case of error
+                callback(reviewList)
             }
         })
     }
 
-    //TODO get All review Using RESTApi and PostGreSQl
-
     private fun getAllReviewsFromRestApi(callback: (ArrayList<Review>?, Throwable?) -> Unit) {
         val apiService = RetrofitClient.getApiService()
-
         val call: Call<ResponseBody> = apiService.getAllReviews()
 
         call.enqueue(object : Callback<ResponseBody> {
@@ -197,13 +140,11 @@ class AboutUsActivity : AppCompatActivity() {
                     val reviews = parseReviewsFromJson(rawJson)
                     callback(reviews, null)
                 } else {
-                    Log.e("getAllReviewsFromRestApi", "Failed to fetch reviews: ${response.code()}")
                     callback(null, Exception("Failed to fetch reviews: ${response.code()}"))
                 }
             }
 
             override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                Log.e("getAllReviewsFromRestApi", "Error fetching reviews", t)
                 callback(null, t)
             }
         })
@@ -230,23 +171,20 @@ class AboutUsActivity : AppCompatActivity() {
         return reviews
     }
 
-
-    // TODO: get ALL review from PostGreSql using GraphQl
     private fun getAllReviewsFromGraphQl(callback: (ArrayList<Review>?, Throwable?) -> Unit) {
         val query = """
-        query {
-            getReviews {
-                id
-                rating
-                review
-                reviewBy
-                reviewTime
-                profileUri
+            query {
+                getReviews {
+                    id
+                    rating
+                    review
+                    reviewBy
+                    reviewTime
+                    profileUri
+                }
             }
-        }
-    """.trimIndent()
+        """.trimIndent()
 
-        //val variables = hashMapOf<String, Any>()
         val service = RetrofitClientForGraphQl.getApiService()
         val call = service.postQuery(GraphQLRequest(query, emptyMap()))
 
@@ -272,19 +210,13 @@ class AboutUsActivity : AppCompatActivity() {
 
                     callback(reviewList, null)
                 } else {
-                    Log.e("getAllReviewsFromGraphQl", "${response.errorBody()}")
                     callback(null, Exception("Failed to fetch reviews: ${response.code()}"))
                 }
             }
 
             override fun onFailure(call: Call<JsonObject>, t: Throwable) {
-                Log.e("getAllReviewsFromGraphQl", "Error fetching reviews", t)
                 callback(null, t)
             }
         })
     }
-
 }
-
-
-
